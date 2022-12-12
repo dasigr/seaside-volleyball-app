b84fe48ef444f8c33f9a4e4f52a50af409a2e9de
Romualdo Dasig <dasig.rg@a5project.com>
2022-12-12 08:53:44 +0800

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Video from './pages/Video';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Advertisement from './components/advertisement/Advertisement';
-import LatestNews from './components/latest-news/LatestNews';
-import UpcomingEvents from './components/upcoming-events/UpcomingEvents';
-import TeamStandings from './components/team-standings/TeamStandings';
 
 import './App.css';
 
 const App = () => (
-  <Router>
+  <BrowserRouter>
     <div className="page">
       <Header></Header>
       <section className="main">
@@ -25,7 +21,7 @@ const App = () => (
       </section>
       <Footer></Footer>
     </div>
-  </Router>
+  </BrowserRouter>
 );
 
 export default App;
